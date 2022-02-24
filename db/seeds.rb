3193b826cb64ca76d7596966c12bc445c7828d25
donellgill <donell.gill@outlook.com>
2022-02-24 15:25:33 -0500

    generated Task resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Task.create!(text: 'Take out the trash', isCompleted: false)
+Task.create!(text: 'Wash the dishes.', isCompleted: false )
+Task.create!(text: 'Do laundry.', isCompleted: false )
+Task.create!(text: 'Clean kitchen.', isCompleted: false )
+Task.create!(text: 'Blow out leaves.', isCompleted: false )
+Task.create!( text: "Learn about React", isCompleted: false )
+Task.create!( text: "Meet friend for lunch", isCompleted: false )
+Task.create!( text: "Build really cool todo app", isCompleted: false )
