b1a3bebc5ee5515b1ad6a941f0ec165c48f1546e
donellgill <donell.gill@outlook.com>
2022-02-24 15:32:37 -0500

    created index and show actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,20 @@ class TasksController < ApplicationController
         tasks = Task.all
         render json: tasks
     end
+
+    def show
+        task = Task.find(task_params[:id])
+        if task
+            render json: task
+        else
+            render json: {error: "Task not found" }, status: :not_found
+        end
+    end
+
+    private
+
+    def task_params
+        params.permit(:text, :isCompleted, :id)
+    end
+
 end
