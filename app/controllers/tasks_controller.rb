eee92a6ea88f391456346222549e4287813f788e
donellgill <donell.gill@outlook.com>
2022-02-24 23:35:09 -0500

    created update action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,11 @@
 class TasksController < ApplicationController
+    #Get all tasks - /tasks
     def index
         tasks = Task.all
         render json: tasks
     end
 
+    #Get a particular task - /tasks/:id
     def show
         task = Task.find(task_params[:id])
         if task
@@ -13,11 +15,22 @@ class TasksController < ApplicationController
         end
     end
 
+    #Create a new task - Post /tasks
     def create
         task = Task.create(task_params)
         render json: task
     end
-    
+
+    #Find a task and edit - Patch /tasks/:id
+    def update
+        task = Task.find(task_params[:id])
+        if task
+            task.update(task_params)
+            render json: task
+        else 
+            render json: {error: "Task not found" }, status: :not_found
+        end
+    end
 
     private
 
